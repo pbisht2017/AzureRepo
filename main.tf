33d792a2d6de0c77b1942744f2932bad66afd2b0
prateek bisht <prateeksbisht2025@gmail.com>
2021-07-05 14:28:12 +0530

    updated code 6

--- main.tf
+++ main.tf
@@ -1,20 +1,57 @@
+# PROVIDER
 provider "azurerm" {
   version = "2.32.0"
 
-subscription_id = "3caab25b-163a-4864-8816-20f9af09d1a0"
-client_id       = "c577b289-d737-4707-a4e0-36b4f8fe3682"
-client_secret   = "8SCAGmSkxk~6VhD2bk7-G9ajq9w._xROZ6"
-tenant_id       = "c5ed1c03-8dc8-4e79-822c-529139a7f3ce"
+skip_provider_registration = true
   
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-    name = "dockerRG"
-    location = "East US"
+##-------------------VIRTUAL NETWORK SETUP WITH SUBNETS AND PEERING-----------------------##
 
-    tags = {
-      "Environment" = "production"
-    }
-  
+module "vnet_module1" {
+  source = "../modules/azure-vnet"
+  virtual_network_name                = var.virtual_network_name
+  virtual_network_address_space       = var.address_space
+  resource_group_location             = var.resource_group_location
+  resource_group_name                 = var.resource_group_name
+  subnet_address_prefix               = var.address_prefix
+  vnet_environment_name_tag           = var.vn1environment_name_tag
+}
+module "vnet_module2" {
+  source = "../modules/azure-vnet"
+  virtual_network_name                = var.virtual_network1_name
+  virtual_network_address_space       = var.address_space_vnet2
+  resource_group_location             = var.resource_group_location
+  resource_group_name                 = var.resource_group_name
+  subnet_address_prefix               = var.address_prefixvnet2
+  vnet_environment_name_tag           = var.vn2environment_name_tag
+}
+
+
+##-----------------------Create Virtual Machine----------------------------------------------------##
+
+
+module "jumphost" {
+  source                            = "../modules/azure-vm"
+  armvm_name                        = var.vm_jumphost_name 
+  armvm_nic_name                    = var.vm_jumphost_nicname
+  armvm_location                    = var.resource_group_location
+  armvm_resgrp_name                 = var.resource_group_name
+  armvm_size                        = var.vm_jumphost_size
+  armvm_subnetid                    = module.vnet_module1.subnets_id[4]
+  armvm_pvtipaddr_alloc             = var.vm_jumphost_pvtipaddr_allocation
+  vm_osdisk_caching                 = var.vm_jumphost_osdisk_caching
+  vm_osdisk_crtopt                  = var.vm_jumphost_osdisk_crtopt
+  vm_managedosdisk_type             = var.vm_jumphost_managedosdisk_type
+  vm_osdisk_boottype                = var.vm_jumphost_osdisk_boottype
+  vm_osdisk_size                    = var.vm_jumphost_osdisk_size
+  vm_ddisk_0_size                   = var.vm_jumphost_ddisk_0_size
+  vm_managedddisk_0_type            = var.vm_jumphost_managedddisk_0_type
+  armvm_hostname                    = var.vm_jumphost_name
+  /*armvm_hostadmin_name              = var.vm_jumphost_hostadmin_name
+  armvm_hostadmin_pwd               = var.vm_jumphost_hostadmin_pwd*/
+  armvm_name_tag                    = var.vm_jumphost_nametag
+  armvm_nicname_tag                 = var.vm_jumphost_nicnametag
+
 }
