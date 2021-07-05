4fd4043003d1d0442f37a1c240937e9006459a92
prateek bisht <prateeksbisht2025@gmail.com>
2021-07-05 14:39:30 +0530

    updated code 9

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
       resource_group_name = "dockerRG"
-      storage_account_name = "terrastorage2289"
+      storage_account_name = "terrastorage22894"
       container_name = "development"
       key = "development.tfstate"
-      access_key = "8Gutdl7HnjfhgaFYqY5wiRb8GZphQK0ltcBo7f5qBjOrUzTZoUX/6cxqiyhsQ/4j9/lHVqCgOFLtlleXibHeRw=="
+      access_key = "uhpPRiLi8IYyumt4w+ZsLsO0Oo9Jn+R8+Pfjp2Q957rBcabRy6i+FSygmoUJq4mzB32kNwKtoRMUGyevIgjtSg=="
     
   }
 }
