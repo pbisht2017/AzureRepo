e7d129fa4c0d8707cf52ae197edc9e45180f0610
prateek bisht <prateeksbisht2025@gmail.com>
2021-07-05 10:18:14 +0530

    updated code2

--- main.tf
+++ main.tf
@@ -3,8 +3,10 @@ provider "azurerm" {
   version = "2.32.0"
 
 subscription_id = "3caab25b-163a-4864-8816-20f9af09d1a0"
-skip_provider_registration = true
-  
+client_id       = "c577b289-d737-4707-a4e0-36b4f8fe3682"
+client_secret   = "8SCAGmSkxk~6VhD2bk7-G9ajq9w._xROZ6"
+tenant_id       = "c5ed1c03-8dc8-4e79-822c-529139a7f3ce" 
+
   features {}
 }
 
