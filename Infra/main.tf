6447bd4b9c0b9eb9764659da02551ee5d5479764
Prateek <prateeksbisht2017@gmail.com>
2025-05-25 14:37:42 +0530

    update Infra dir

--- Infra/main.tf
+++ Infra/main.tf
@@ -99,7 +99,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "22_04-lts"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 }
