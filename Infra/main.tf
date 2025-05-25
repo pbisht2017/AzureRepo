f8d407d9b59beed14e7feb305b90aaa15635f015
Prateek <prateeksbisht2017@gmail.com>
2025-05-25 13:02:31 +0530

    update Infra dir

--- Infra/main.tf
+++ Infra/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_security_group" "nsg" {
